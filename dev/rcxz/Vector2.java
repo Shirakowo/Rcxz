0b2a19f3fd486032e00f72e7b8d0e4d1b7b9f930
Shirako <shirako.cs@gmaill.com>
2025-01-19 18:08:40 +0800

    Add four arithmetic operations to Vector2

--- dev/rcxz/Vector2.java
+++ dev/rcxz/Vector2.java
@@ -11,3 +11,31 @@ public class Vector2 {
         this.y = y;
     }
 
+    public Vector2 add(Vector2 other) {
+        return new Vector2(this.x + other.x, this.y + other.y);
+    }
+
+    public Vector2 add(int x, int y) {
+        return new Vector2(this.x + x, this.y + y);
+    }
+
+    public Vector2 subtract(Vector2 other) {
+        return new Vector2(this.x - other.x, this.y - other.y);
+    }
+
+    public Vector2 subtract(int x, int y) {
+        return new Vector2(this.x - x, this.y - y);
+    }
+
+    public Vector2 multiply(int scalar) {
+        return new Vector2(this.x * scalar, this.y * scalar);
+    }
+
+    public Vector2 divide(int scalar) {
+        if (scalar == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        
+        return new Vector2(this.x / scalar, this.y / scalar);
+    }
+}
